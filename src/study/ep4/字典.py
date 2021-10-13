2b239d98ada681c23b3fea57ba9192b370162d26
ToSeeAll <qwer2846579@gmail.com>
2021-10-13 19:40:41 +0800

    字典学了一丢丢

--- src/study/ep4/字典.py
+++ src/study/ep4/字典.py
@@ -1,3 +1,5 @@
+import sys
+
 dict1 = {'a': 1, 'b': 2, 'c': 3}
 print(type(dict1))
 print(dict1)
@@ -10,3 +12,19 @@ for k in dict1:
 print(dict1.items())
 for (a, b) in dict1.items():
     print('dict[%s]' % a, b)
+print(dict1.keys())
+print(dict1.values())
+# 浅拷贝
+print("##########################")
+dict12 = {'d': 23, 'c': 12}
+dict12 = dict1.copy()
+print(dict1, '\n', dict12)
+dict1['a'] = 43
+print(dict1, '\n', dict12)
+dict12['b'] = 333
+print(dict1, '\n', dict12)
+
+# 全局字典
+print(sys.modules.keys())
+print(sys.modules.values())
+print(sys.modules["os"])
